bf35cdcee05ce70b5ca6b8528535b15fa581133d
Calderon0723 <00215818@uca.edu.sv>
2025-10-31 11:58:11 -0600

    UPDATE: Global Styles :bug:

--- Frontend_PokedexV2_React/vite.config.ts
+++ Frontend_PokedexV2_React/vite.config.ts
@@ -22,3 +22,37 @@ export default defineConfig({
     },
   },
 });
+
+
+/*Alternativa:
+*
+* import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "src"),
+      "@assets": path.resolve(__dirname, "src/assets"),
+      "@components": path.resolve(__dirname, "src/components"),
+      "@context": path.resolve(__dirname, "src/contexts"),
+      "@data": path.resolve(__dirname, "src/data"),
+      "@hooks": path.resolve(__dirname, "src/hooks"),
+      "@models": path.resolve(__dirname, "src/models"),
+      "@pages": path.resolve(__dirname, "src/pages"),
+      "@routes": path.resolve(__dirname, "src/routes"),
+      "@services": path.resolve(__dirname, "src/services"),
+      "@styles": path.resolve(__dirname, "src/styles"),
+      "@types": path.resolve(__dirname, "src/types"),
+      "@utils": path.resolve(__dirname, "src/utils"),
+    },
+  },
+});
+*
+* */
